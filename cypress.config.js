504e737eca16e4fd1af9af1d4b98e7295b66c6ad
Viviana Nieto <nietoviviana1075@gmail.com>
2026-07-11 04:15:48 -0500

    Sonarqube

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
   e2e: {
+    baseUrl: 'http://localhost:8081',
+    video: true,
+    videoCompression: 32,
+    screenshotOnRunFailure: true,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
   },
 });
